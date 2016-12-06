f4253fd1a95c03c1cf9128d6c888192ea1934e07
Mohamed.k.Khalil <mo7amd.khaled@gmail.com>
2016-12-06 15:12:29 +0200

    add project file

---
+++ final_project.m
@@ -0,0 +1,67 @@
+function final_project ()
+
+    prompt = {'Enter number of sides','Enter vertices: '};
+    
+    %     for i = 1:num_of_sides 
+%        fprintf('enter the vertics %d',i);
+%        prompt = (' :');
+%        position = input(prompt);
+%        x(1,i) = position(1,1);
+%        y(1,i) = position(1,2);
+%     end
+    
+    dlg_title = 'Input';
+    num_lines = 5;
+    
+    position = inputdlg(prompt,dlg_title,num_lines);
+    num_of_sides = str2num(num_of_sides{1});
+
+    x(1,num_of_sides+1) = x(1,1);
+    y(1,num_of_sides+1) = y(1,1);
+    subplot(3,1,1); 
+    plot(x, y, 'b-', 'LineWidth', 3);
+%     ************************************************************
+    while true 
+    prompt = ('select the oepration : \n 1) rotate CCW \n 2) rotate CC\n 3) flip horizontal\n 4) flip vertical \n :  ');
+    operation = input(prompt);
+    switch operation 
+        case 1
+            [x, y] = rotate_90_ccw(x,y);
+        case 2
+            [x, y] = rotate_90_cc(x,y);
+        case 3
+            [x,y] = flip_h(x,y);
+        case 4
+            [x,y] = flip_v(x,y);
+        end
+        
+    end
+end 
+
+function [x_rot,y_rot] =  rotate_90_ccw (x,y)
+    x_rot = -1.*y;
+    y_rot = x;
+    subplot(3,1,2);
+    plot(x_rot, y_rot, 'b-', 'LineWidth', 3);
+end
+
+function [x_rot,y_rot] = rotate_90_cc (x,y) 
+    x_rot = y;
+    y_rot = -1.*x;
+    subplot(3,1,2);
+    plot(x_rot, y_rot, 'b-', 'LineWidth', 3);
+end
+
+function [x_flip,y_flip] = flip_h(x,y)
+   x_flip = -1.*x;
+   y_flip = y;
+   subplot(3,1,2);
+   plot(x_flip,y_flip, 'b-', 'LineWidth', 3);
+end
+
+function [x_flip,y_flip] = flip_v(x,y)
+   x_flip = x;
+   y_flip = -1.*y;
+   subplot(3,1,2);
+   plot(x_flip,y_flip, 'b-', 'LineWidth', 3);
+end
